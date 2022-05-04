f87dfd06bd1e6695d7892159b8407fc0cdd081a7
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-05-04 19:12:04 -0400

    fixed script placement add imgs

--- assets/JS/alert.js
+++ assets/JS/alert.js
@@ -23,3 +23,19 @@ window.onload = function() {
     element.appendChild(para);
 }
 
+let img = document.querySelector('img');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+
+btn1.addEventListener('click', () => {
+    img.src = './assets/images/roasted.jpg';
+})
+
+btn2.addEventListener('click', () => {
+    img.src = './assets/images/salsa.jpg';
+})
+
+btn3.addEventListener('click', () => {
+    img.src = './assets/images/plated.jpg';
+})
